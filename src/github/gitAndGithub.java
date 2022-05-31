3be7d49f8bac9e7b17bd28b11005687cabe17156
natig77 <natiggurbanov@gmail.com>
2022-05-31 19:54:26 -0400

    2nd commit

--- src/github/gitAndGithub.java
+++ src/github/gitAndGithub.java
@@ -3,6 +3,7 @@ package github;
 public class gitAndGithub {
 	public static void main(String[] args) {
 		System.out.println("today we learn github");
+		System.out.println("today is a very good day");
 	}
 
 }
